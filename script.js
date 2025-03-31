57b65147e31c57503b59cd222400e8ca1f283f03
koshal-4236 <koshalk3902@gmail.com>
2025-03-31 17:08:11 +0530

    Completed

--- script.js
+++ script.js
@@ -3,17 +3,28 @@ toolBtns=document.querySelectorAll(".tool"),
 fillColor=document.querySelector("#fill-color"),
 sizeSlider=document.querySelector("#size-slider"),
 colorBtns=document.querySelectorAll(".colors .option"),
+colorPicker=document.querySelector("#color-picker"),
+clearCanvas=document.querySelector(".clear-canvas"),
+saveImg=document.querySelector(".save-img"),
 ctx=canvas.getContext("2d");
 
 let preMouseX,preMouseY,snapshot,
 isDrawing=false,
 selectedTool="brush",
-brushWidth=5;
+brushWidth=5,
 selectedColor="#000";
 
+
+const setCanvasBackground=()=>{
+    ctx.fillStyle="#fff";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle=selectedColor;
+    
+}
 window.addEventListener("load",()=>{
     canvas.width=canvas.offsetWidth;
     canvas.height=canvas.offsetHeight;
+    setCanvasBackground();
 });
 
 const drawRect=(e)=>{
@@ -45,13 +56,16 @@ const startDraw=(e)=>{
     preMouseY=e.offsetY;
     ctx.beginPath();
     ctx.lineWidth=brushWidth;
+    ctx.strokeStyle=selectedColor;
+    ctx.fillStyle=selectedColor;
     snapshot=ctx.getImageData(0,0,canvas.width,canvas.height);
 }
 const drawing=(e)=>{
     if(!isDrawing) return;
     ctx.putImageData(snapshot,0,0);
 
-    if(selectedTool==="brush"){
+    if(selectedTool==="brush" || selectedTool=="eraser"){
+        ctx.strokeStyle=selectedTool==="eraser"?"#fff":selectedColor;
     ctx.lineTo(e.offsetX,e.offsetY);
     ctx.stroke();
     }
@@ -84,10 +98,27 @@ colorBtns.forEach(btn=>{
     btn.addEventListener("click",()=>{
         document.querySelector(".options .selected ").classList.remove("selected");
         btn.classList.add("selected");
-        console.log(window.getComputedStyle(btn).getPropertyValue("background-color"));
+        selectedColor=window.getComputedStyle(btn).getPropertyValue("background-color");
     });
 });
 
+colorPicker.addEventListener("change",()=>{
+    colorPicker.parentElement.style.background=colorPicker.value;
+    colorPicker.parentElement.click();
+});
+
+clearCanvas.addEventListener("click",()=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    setCanvasBackground();
+});
+
+
+saveImg.addEventListener("click",()=>{
+    const link=document.createElement("a");
+    link.download=`${Date.now()}.jpg`;
+    link.href=canvas.toDataURL();
+    link.click();
+});
 canvas.addEventListener("mousedown",startDraw);
 canvas.addEventListener("mousemove",drawing);
 canvas.addEventListener("mouseup",()=>isDrawing=false);
